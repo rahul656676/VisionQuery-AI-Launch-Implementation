735972ebd4788c86a5a6f66aff0086244a92b49e
rahul656676 <rahul656676@users.noreply.github.com>
2026-07-27 15:26:21 +0530

    Fix CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(SlowAPIMiddleware)
 # Set up CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
